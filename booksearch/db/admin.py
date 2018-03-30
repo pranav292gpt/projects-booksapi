7abe6394eba3b7217a7ecd7cf9889b47adcede04
Po <Pranav>
2018-03-30 12:15:51 +0530

    Added admin

--- booksearch/db/admin.py
+++ booksearch/db/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from models import User, Search
+
+class SearchAdmin(admin.ModelAdmin):
+    search_fields = ['request', 'response']
+
+admin.site.register(User)
+admin.site.register(Search, SearchAdmin)
